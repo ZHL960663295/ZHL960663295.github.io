a260b258c5833cdb872e485452dbda0b29d210e1
ZHL960663295 <960663295@qq.com>
2023-06-13 11:53:22 +0800

    fix:测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -75,11 +75,11 @@ module.exports = {
             },
             {
                 title: "遇到的问题",
-                path: '/handbook/ConditionalTypes',
+                path: '/ques/VueQuestion',
                 collapsable: false, // 不折叠
                 children: [{
                     title: "自定义指令问题",
-                    path: "/handbook/ConditionalTypes"
+                    path: "/ques/VueQuestion"
                 }
             ],
             }
--- docs/ques/VueQuestion.md
+++ docs/ques/VueQuestion.md
@@ -3,3 +3,4 @@ title: vue开发过程中遇到的问题
 author: 一点
 date: '2023-06-19'
 ---
+fsdrfsd 
